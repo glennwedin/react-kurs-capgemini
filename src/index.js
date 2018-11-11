bf4bf9469d45e9abce7173f5d759b7bb701dcc40
Glenn Wedin <glenn.wedin@gmail.com>
2018-11-11 14:45:08 +0100

    update deps and configs to latest

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
+/* global document */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HashRouter, Switch, Route } from 'react-router-dom';
-import "babel-polyfill";
+import '@babel/polyfill';
 import Main from './containers/main';
 import store from './configureStore';
-import "!style-loader!css-loader!sass-loader!./style/style.scss";
+import '!style-loader!css-loader!sass-loader!./style/style.scss';
 
 const router = (
-	<Provider store={store}>
-		<HashRouter>
-			<Switch>
-				<Route exact path="/" component={Main} />
-				<Route path="/test" render={() => <div>Dette er en underside</div>} />
-			</Switch>
-		</HashRouter>
-	</Provider>
+    <Provider store={store}>
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" component={Main} />
+                <Route
+                    path="/test"
+                    render={() => <div>Dette er en underside</div>}
+                />
+            </Switch>
+        </HashRouter>
+    </Provider>
 );
 
 ReactDOM.render(router, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,45 @@
 var webpack = require('webpack');
 
 module.exports = {
-	entry: [
-		"./src/index.js",
-	],
-	output: {
-		path: __dirname + "/dist",
-		filename: "app.js"
-	},
-	module: {
-		loaders: [{
-			test: /\.scss$/,
-			use: [{
-                loader: "style-loader" // creates style nodes from JS strings
-            }, {
-                loader: "css-loader" // translates CSS into CommonJS
-            }, {
-                loader: "sass-loader" // compiles Sass to CSS
-            }]
-		}, {
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: 'babel-loader',
-			query: {
-				presets: ["react", "env"]
-			}
-		}]
-	}
-}
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader' // creates style nodes from JS strings
+                    },
+                    {
+                        loader: 'css-loader' // translates CSS into CommonJS
+                    },
+                    {
+                        loader: 'sass-loader' // compiles Sass to CSS
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-react',
+                            [
+                                '@babel/preset-env',
+                                {
+                                    useBuiltIns: 'entry'
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
